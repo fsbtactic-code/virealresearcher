385a87e35449419307cfb2a6b1beb561de2e2e0f
fsbtactic-code <fsbtactic@gmail.com>
2026-04-21 03:45:03 +0300

    fix(ui): remove orphan nav block (Banana/Master/Support) from scroll-area; clean form text

--- ui_templates/_verify.py
+++
@@ -1,7 +0,0 @@
-"""Verify the form restoration."""
-html = open('ui_templates/launcher.html', 'r', encoding='utf-8').read()
-print('configForm present:', ('id="configForm"' in html))
-print('scrape_explore present:', ('scrape_explore' in html))
-print('startBtn present:', ('startBtn' in html))
-print('authStatusBox present:', ('authStatusBox' in html))
-print('Total bytes:', len(html.encode('utf-8')))
